f6991a6a4087005d5f7428f6b73ad4f48e9b5ce4
vororo <adamolsson@outlook.com>
2023-11-16 15:13:33 +0900

    feat: Implemented !spell:name command to get spell info from api

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"math/rand"
+	"net/http"
 	"os"
 	"regexp"
 	"strconv"
@@ -85,33 +87,58 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 	if m.Content == BotPrefix+"ping" {
 		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
-	} else if strings.Contains(m.Content, "roll:") { //I want to parse a command like roll:1d6 or roll:10d20
-		expRes := regexp.MustCompile("[0-9]+")
-		result := expRes.FindAllStringSubmatch(m.Content, -1)
-
-		rolls, err := strconv.Atoi(result[0][0])
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		rollType, err := strconv.Atoi(result[1][0])
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		message := "You rolled:"
-		total := 0
-
-		for i := 0; i < rolls; i++ {
-			roll := rand.Intn(rollType + 1)
-			total += roll
-			message += " " + strconv.Itoa(roll) + ","
-		}
-
-		message += " = " + strconv.Itoa(total)
-		_, _ = s.ChannelMessageSend(m.ChannelID, message)
+	} else if strings.Contains(m.Content, "roll:") { //parsing commands like this roll:1d6
+		rollDice(s, m)
+	} else if strings.Contains(m.Content, "spell:") {
+		getSpell(s, m)
 	}
+}
+
+func rollDice(s *discordgo.Session, m *discordgo.MessageCreate) {
+	expRes := regexp.MustCompile("[0-9]+")
+	result := expRes.FindAllStringSubmatch(m.Content, -1)
+
+	rolls, err := strconv.Atoi(result[0][0])
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	rollType, err := strconv.Atoi(result[1][0])
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	message := "You rolled:"
+	total := 0
+
+	for i := 0; i < rolls; i++ {
+		roll := rand.Intn(rollType + 1)
+		total += roll
+		message += " " + strconv.Itoa(roll) + ","
+	}
+
+	message += " = " + strconv.Itoa(total)
+	_, _ = s.ChannelMessageSend(m.ChannelID, message)
+}
+
+func getSpell(s *discordgo.Session, m *discordgo.MessageCreate) {
+	target := strings.ReplaceAll(m.Content, "!spell:", "")
+
+	resp, err := http.Get("https://www.dnd5eapi.co/api/spells/" + target)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	sb := string(body)
+	_, _ = s.ChannelMessageSend(m.ChannelID, sb)
 }
 
 func main() {
